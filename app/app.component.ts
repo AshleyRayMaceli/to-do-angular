73eb8d558d3b0a13f095e9290e404fe38d4814a2
Ashley Maceli and Nadiya Yeroshkina <nadiya@>
2016-05-31 15:40:33 -0700

    add TaskComponent (child of TaskListComponent)

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,19 +1,40 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
+
+@Component({
+    selector: 'task-display',
+    inputs: ['task'],
+  template: `
+    <h3>{{ task.description }}</h3>
+  `
+})
+export class TaskComponent {
+  public task: Task;
+}
 
 @Component({
   selector: 'task-list',
   inputs: ['taskList'],
+  outputs: ['onTaskSelect'],
+  directives: [TaskComponent],
   template: `
-  <h3 *ngFor="#currentTask of taskList" (click)="taskClicked(currentTask)">
-    {{ currentTask.description }}
-  </h3>
+  <task-display *ngFor="#currentTask of taskList"
+    (click)="taskClicked(currentTask)"
+    [class.selected]="currentTask === selectedTask"
+    [task]="currentTask">
+  </task-display>
   `
 })
-
 export class TaskListComponent {
   public taskList: Task[];
+  public onTaskSelect: EventEmitter<Task>;
+  public selectedTask: Task;
+  constructor() {
+    this.onTaskSelect = new EventEmitter();
+  }
   taskClicked(clickedTask: Task): void {
-    console.log(clickedTask);
+    console.log('child', clickedTask);
+    this.selectedTask = clickedTask;
+    this.onTaskSelect.emit(clickedTask);
   }
 }
 
@@ -21,13 +42,15 @@ export class TaskListComponent {
   selector: 'my-app',
   directives: [TaskListComponent],
   template: `
-    <div class="container">
-      <h1>To-Do List</h1>
-      <task-list [taskList]="tasks"></task-list>
-    </div>
-  `
+  <div class="container">
+       <h1>To-Do List</h1>
+       <task-list
+         [taskList]="tasks"
+         (onTaskSelect)="taskWasSelected($event)">
+       </task-list>
+     </div>
+   `
 })
-
 export class AppComponent {
   public tasks: Task[];  // Task[] (or Array<Task>) identifies tasks as an array of Task objects
   constructor(){
@@ -39,7 +62,7 @@ export class AppComponent {
     ];
   }
   taskWasSelected(clickedTask: Task): void {
-    console.log(clickedTask);
+    console.log('parent', clickedTask);
   }
 }
 
